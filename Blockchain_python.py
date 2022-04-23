5ce355867a0697d07b60e5ae6e7385b8d0005c31
felixironwinter <tjwinterflood@gmail.com>
2022-04-23 17:18:13 +0100

    v2_blockchain

--- Blockchain_python.py
+++ Blockchain_python.py
@@ -1,18 +1,13 @@
 
 import hashlib
-print ("hello world")
-
-
 
 class TobyCoinBlock:
-
     def __init__(self, previous_block_hash , transaction_list):
         self.previous_block_hash = previous_block_hash
         self.transaction_list = transaction_list
 
         self.block_data = f"{' - '.join(transaction_list)} - {previous_block_hash}"
         self.block_hash = hashlib.sha256(self.block_data.encode()).hexdigest()
-
 
 class Blockchain:
     def __init__(self):
@@ -35,28 +30,12 @@ class Blockchain:
     def last_block(self):
         return self.chain[-1]
 
-#t1 = "Noah sends 5 GC to Mark"
-#t2 = "Mark sends 2.3 GC to James"
-#t3 = "James sends 4.2 GC to Alisson"
-#t4 = "Alisson sends 1.1 GC to Noah"
-
-#block1 = TobyCoinBlock('firstblock',[t1, t2])
-
-#print(f"Block 1 data: {block1.block_data}")
-#print(f"Block 1 hash: {block1.block_hash}")
-
-#block2 = TobyCoinBlock(block1.block_hash, [t3, t4])
-
-#print(f"Block 2 data: {block2.block_data}")
-#print(f"Block 2 hash: {block2.block_hash}")
-
 t1 = "George sends 3.1 GC to Joe"
 t2 = "Joe sends 2.5 GC to Adam"
 t3 = "Adam sends 1.2 GC to Bob"
 t4 = "Bob sends 0.5 GC to Charlie"
 t5 = "Charlie sends 0.2 GC to David"
 t6 = "David sends 0.1 GC to Eric"
-
 
 myblockchain = Blockchain()
 
